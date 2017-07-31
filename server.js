5a00739db47727e58743cb3a38780604878b607b
Austin Cohrs <acohrsdev@outlook.com>
2017-07-31 14:12:43 -0700

    Mob programming the server and database setup.

--- server.js
+++ server.js
@@ -1,28 +1,56 @@
 'use strict';
 
+const pg = require('pg');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const conString = 'postgres://postgres:1Bash2Bash0110!:@localhost:5432';
+const client = new pg.client(conString);
+client.connect();
+client.on('error', err => console.error(err));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('./'));
+app.use(express.static('./public'));
 
 app.get('', function(request, response) {
   response.sendfile('index.html', {root: './'})
 });
 
-function proxyGitHub(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
-  (requestProxy({
-    url: `https://api.github.com/${request.params[0]}`,
-    headers: {Authorization: `token ad24cd1a1ac4847d98a5c93319a6285210016ac3`}
-  }))(request, response);
-}
+// function proxyGitHub(request, response) {
+//   console.log('Routing GitHub request for', request.params[0]);
+//   (requestProxy({
+//     url: `https://api.github.com/${request.params[0]}`,
+//     headers: {Authorization: `token ad24cd1a1ac4847d98a5c93319a6285210016ac3`}
+//   }))(request, response);
+// }
 
-app.get('/github/*', proxyGitHub);
+// app.get('/github/*', proxyGitHub);
+let currentUser = JSON.parse(localStorage.user);
+app.get('/notes', (request, response) => {
+  client.query(`
+    SELECT notes FROM `${currentUser}`;`
+  )
+  .then(result => response.send(result.rows))
+  .catch(console.error);
+});
 
 app.listen(PORT, function() {
   console.log(`'Listening on port: ${PORT}'`);
 });
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    ${currentUser} (
+      note_id SERIAL PRIMARY KEY,
+      subject VARCHAR(255) NOT NULL,
+      "publishedOn" DATE,
+      body TEXT NOT NULL
+    );`
+  )
+  .then(loadNotes)
+  .catch(console.error);
+}
