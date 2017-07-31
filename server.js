0628cd0266bfaac6581eda6a21d656075c817f6a
Austin Cohrs <acohrsdev@outlook.com>
2017-07-31 16:47:58 -0700

    Server refactor

--- server.js
+++ server.js
@@ -19,40 +19,6 @@ app.get('', function(request, response) {
   response.sendfile('index.html', {root: './public'})
 });
 
-// function proxyGitHub(request, response) {
-//   console.log('Routing GitHub request for', request.params[0]);
-//   (requestProxy({
-//     url: `https://api.github.com/${request.params[0]}`,
-//     headers: {Authorization: `token ad24cd1a1ac4847d98a5c93319a6285210016ac3`}
-//   }))(request, response);
-// }
-
-
-// app.get('/github/*', proxyGitHub);
-// app.get('/notes', (request, response) => {
-//   let currentUser = app.user;
-//   client.query(`
-//     SELECT notes FROM ${currentUser};`
-//   )
-//   .then(result => response.send(result.rows))
-//   .catch(console.error);
-// });
-
 app.listen(PORT, function() {
   console.log(`'Listening on port: ${PORT}'`);
 });
-
-function loadDB() {
-  let currentUser = app.user;
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    ${currentUser} (
-      note_id SERIAL PRIMARY KEY,
-      subject VARCHAR(255) NOT NULL,
-      "publishedOn" DATE,
-      body TEXT NOT NULL
-    );`
-  )
-  .then(loadNotes)
-  .catch(console.error);
-}
