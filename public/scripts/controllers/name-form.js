cb7f2b7724ec023b912a0e1e8b741d126e242349
Brendan Davis <brendanmd@gmail.com>
2017-08-02 16:11:54 -0700

    If localStorage has a github username for the user the form will be removed. This includes if the user submits one

--- public/scripts/controllers/name-form.js
+++ public/scripts/controllers/name-form.js
@@ -6,8 +6,12 @@ function readUser(e) {
   var gitUser = userForm.gitUN.value;
   localStorage.setItem('user', gitUser);
   console.log(gitUser + ' recorded as the user.')
+  $('#gitForm').hide()
 }
 
 
+if (localStorage.user !== undefined) {
+  $('#gitForm').hide()
+}
 
 $('#git-button').on('click', readUser)
