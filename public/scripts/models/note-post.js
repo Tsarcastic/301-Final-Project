368929127c02c4c051f433fd17fde61dd8955938
Brendan Davis <brendanmd@gmail.com>
2017-08-03 14:45:39 -0700

    hide and show notes

--- public/scripts/models/note-post.js
+++ public/scripts/models/note-post.js
@@ -2,8 +2,10 @@
 var app = app || {};
 
 (function(module) {
+
+
+
   let currentUser = localStorage.getItem('user');
-  console.log(currentUser);
 
   function User(currentUser, title, category, body) {
     this.userName = currentUser;
@@ -15,31 +17,37 @@ var app = app || {};
 
   User.prototype.createUser = function(callback) {
     $.post('/user', {
-        userName: this.userName,
-        title: this.title,
-        category: this.category,
-        body: this.body
-      })
-      .then(console.log)
-      .then(callback);
+      userName: this.userName,
+      title: this.title,
+      category: this.category,
+      body: this.body
+    })
+    .then(console.log)
+    .then(callback);
   };
   let userNotes = [];
+
   const successCallback = function(data) {
     userNotes = data;
     console.log(userNotes);
     User.renderNotes();
+    User.renderAll();
+    $('#allNotes').hide()
   }
 
   const errorCallback = function(err) {
     console.error(err);
   }
 
+  var fNoteTemplate = Handlebars.compile($('#note-template').html());
+  console.log(userNotes)
+
   User.findWhere = function(userNameField, thisUser, callback) {
     $.get('/notes/find', {
-        field: userNameField,
-        val: thisUser
-      })
-      .then(successCallback, errorCallback)
+      field: userNameField,
+      val: thisUser
+    })
+    .then(successCallback, errorCallback)
   };
   let recentNotes = [];
   User.renderNotes = function() {
@@ -54,8 +62,10 @@ var app = app || {};
         body: userNotes[i].body,
       }))
     }
-
   }
+
+
+
 
 
   // function renderRepos() {
@@ -68,8 +78,32 @@ var app = app || {};
   //     repository: recent
   //   }))
   // }
-
   User.findWhere('user_name', currentUser);
+
+  User.renderAll = function() {
+    let fNoteTemplate = Handlebars.compile($('#fNote-template').html())
+    console.log(userNotes)
+    for (let i = 0; i < userNotes.length; i++) {
+      $('#allNotes').append(fNoteTemplate( {
+        title: userNotes[i].title,
+        category: userNotes[i].category,
+        body: userNotes[i].body,
+      }))
+    }
+  }
+
+  User.switchView = function() {
+    console.log('its listening')
+    if ($('#noteAppend').is(':visible')) {
+      $('#noteAppend').hide();
+      $('#allNotes').show();
+    } else {
+      $('#noteAppend').show();
+      $('#allNotes').hide();
+    }
+  }
+
+$('#noteListener').on('click', User.switchView);
+
   module.User = User;
-  // User.renderNotes();
 })(app);
