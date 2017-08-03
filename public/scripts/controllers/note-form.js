3c45782ea1dee905bbb7918e39ae581356637b27
Brendan Davis <brendanmd@gmail.com>
2017-08-03 11:30:57 -0700

    Checks to see if there is data in the body

--- public/scripts/controllers/note-form.js
+++ public/scripts/controllers/note-form.js
@@ -1,20 +1,23 @@
 'use strict'
 
 function readData(e) {
-  e.preventDefault();
-  let person = localStorage.getItem('user');
-  let title = theForm.title.value;
-  let category = theForm.category.value;
-  let body = theForm.body.value;
-  localStorage.setItem('user', person);
-  let newUser = new app.User(person,title,category
-  ,body);
-  console.log(newUser);
-  newUser.createUser();
-  console.log(person + ' recorded as the user.')
+  if( $('#body').val() === '') {
+    e.preventDefault();
+    console.log('The null value is working');
+    alert('You need to enter notes in the body')
+  } else {
+    let person = localStorage.getItem('user');
+    let title = theForm.title.value;
+    let category = theForm.category.value;
+    let body = theForm.body.value;
+    localStorage.setItem('user', person);
+    let newUser = new app.User(person,title,category
+      ,body);
+      console.log(newUser);
+      newUser.createUser();
+      console.log(person + ' recorded as the user.')
+    }
+  }
 
-}
 
-
-
-$('#post-button').on('click', readData)
+  $('#post-button').on('click', readData)
